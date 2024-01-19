5b434387dc415c9805eba47b131ed0b171d6daf5
2005HAK <hebertkubis15@gmail.com>
2024-01-19 14:55:07 -0300

    first commit

---
+++ PyMavlink/getting_all_dates.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import time
+from pymavlink import mavutil
+
+master = mavutil.mavlink_connection('127.0.0.1:14550')
+
+while True:
+    try:
+        print(master.recv_match().to_dict())
+    except:
+        pass
+    time.sleep(0.1)
