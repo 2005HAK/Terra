0e778413f8135b6ef18da1eba631796ea585bf3e
2005HAK <hebertkubis15@gmail.com>
2024-12-23 15:58:12 -0300

    dar um jeito de usar o mavlink em c++

--- software/raspberry_pi/src/navegation/navegation_5.0/sensors.cpp
+++ software/raspberry_pi/src/navegation/navegation_5.0/sensors.cpp
@@ -1,10 +1,34 @@
+#include <array>
+#include <chrono>
+#include <mavsdk.lib/mavsdk.h>
+#include <mavsdk/plugins/telemetry/telemetry.h>
+
+using namespace std;
+using namespace chrono;
+
 class Sensors{
     private:
-        
+        array<float, 3> acc = {0, 0, 0};
+        array<float, 3> gyro = {0, 0, 0};
+        array<float, 3> mag = {0, 0, 0};
+        array<float, 3> vel = {0, 0, 0};
+        float tempPixhawk;
+        float tempRaspberry;
+        steady_clock::time_point currentTime;
+        steady_clock::time_point oldTime;
 
     public:
         Sensors(){
-            
+            // Conexão
+
+            steady_clock::time_point currentTime = steady_clock::now();
+            steady_clock::time_point oldTime = currentTime;
+
+            sendHeartbeat();
+        }
+
+        void sendHeartbeat(){
+
         }
 
         void updateData(){
