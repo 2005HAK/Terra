857d42cd11e465341f59aa87c4ac2579c6028c76
2005HAK <hebertkubis15@gmail.com>
2024-12-27 17:44:42 -0300

    nada deu certo 2

---
+++ tests/src/pixhawk/test_in_cpp/build/CMakeFiles/pixhawk_usb.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hak/Documents/Terra/tests/src/pixhawk/test_in_cpp/pixhawk_usb.cpp" "/home/hak/Documents/Terra/tests/src/pixhawk/test_in_cpp/build/CMakeFiles/pixhawk_usb.dir/pixhawk_usb.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/mavsdk"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
