9c48aa3764113307e044688786387fb71831e44e
2005HAK <hebertkubis15@gmail.com>
2025-01-03 22:26:36 -0300

    Biblioteca dos motores pronta (precisa de testes), mas não entendi pq a função convertValue só funcionou com double

--- software/raspberry_pi/src/navegation/navegation_5.0/thrusters_control.cpp
+++ software/raspberry_pi/src/navegation/navegation_5.0/thrusters_control.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <array>
+#include <vector>
 #include <string>
 #include <thread>
 #include <chrono>
@@ -14,7 +15,7 @@ using namespace this_thread;
 double POWER_MAX = 25;
 
 //Frequency used for the PWM signal
-int FREQUENCY = 200;
+double FREQUENCY = 200;
 
 /** Thrusters pins
  * 0 - Front left 
@@ -25,9 +26,6 @@ int FREQUENCY = 200;
  * 5 - Back left 
  */
 array<int, 6> PINS = {17, 18, 22, 23, 24, 27};
-
-// PWM form each percentage value
-array <int, 201> PWM_VALUES = {1102, 1107, 1111, 1115, 1119, 1122, 1125, 1128, 1131, 1134, 1137, 1139, 1142, 1145, 1147, 1150, 1152, 1155, 1158, 1160, 1163, 1166, 1169, 1172, 1175, 1178, 1181, 1184, 1187, 1190, 1193, 1196, 1199, 1202, 1205, 1207, 1210, 1212, 1215, 1218, 1220, 1223, 1225, 1228, 1231, 1234, 1237, 1240, 1244, 1247, 1251, 1255, 1258, 1262, 1265, 1268, 1272, 1275, 1278, 1281, 1284, 1288, 1291, 1294, 1298, 1302, 1305, 1309, 1313, 1317, 1321, 1325, 1329, 1333, 1336, 1340, 1344, 1348, 1352, 1355, 1359, 1363, 1368, 1372, 1377, 1382, 1386, 1391, 1396, 1401, 1405, 1410, 1415, 1420, 1426, 1432, 1439, 1447, 1455, 1464, 1500, 1540, 1549, 1557, 1564, 1570, 1576, 1582, 1588, 1593, 1598, 1603, 1608, 1612, 1617, 1621, 1626, 1631, 1635, 1640, 1644, 1648, 1653, 1657, 1661, 1665, 1668, 1672, 1676, 1680, 1684, 1688, 1691, 1695, 1699, 1702, 1706, 1710, 1714, 1717, 1721, 1724, 1727, 1731, 1734, 1737, 1740, 1744, 1747, 1751, 1754, 1758, 1761, 1764, 1767, 1770, 1773, 1776, 1779, 1781, 1784, 1786, 1789, 1791, 1794, 1797, 1800, 1803, 1806, 1809, 1812, 1815, 1818, 1820, 1823, 1825, 1828, 1830, 1833, 1835, 1838, 1840, 1843, 1846, 1849, 1852, 1855, 1858, 1861, 1864, 1867, 1870, 1873, 1876, 1879, 1883, 1886, 1890, 1893, 1897, 1900};
 
 /**
  * @brief Actions the AUV can perform
@@ -71,32 +69,66 @@ enum class Action{
  */
 string actionToString(Action action){
     switch (action){
-        case Action::FORWARD:
-            return "FORWARD";
-        case Action::BACKWARD:
-            return "BACKWARD";
-        case Action::RIGHT:
-            return "RIGHT";
-        case Action::LEFT:
-            return "LEFT";
-        case Action::UP:
-            return "UP";
-        case Action::DOWN:
-            return "DOWN";
-        case Action::TURNRIGHT:
-            return "TURNRIGHT";
-        case Action::TURNLEFT:
-            return "TURNLEFT";
-        case Action::STAY:
-            return "STAY";
-        default:
-            return "UNKNOWN";
+        case Action::FORWARD: return "FORWARD";
+        case Action::BACKWARD: return "BACKWARD";
+        case Action::RIGHT: return "RIGHT";
+        case Action::LEFT: return "LEFT";
+        case Action::UP: return "UP";
+        case Action::DOWN: return "DOWN";
+        case Action::TURNRIGHT: return "TURNRIGHT";
+        case Action::TURNLEFT: return "TURNLEFT";
+        case Action::STAY: return "STAY";
+        default: return "UNKNOWN";
     }
 }
 
+class Thruster{
+    private:
+        int pin, stablePower, currentPower;
+        int pwmRange = 1000;
+
+    public:
+        /**
+         * @brief Initializes the object thruster
+         * 
+         * @param pin pin connected in the thrusters
+         * @param stablePower power for this thurster to stabilize the AUV (Vx: 0, Vy: 0, Vz: 0)
+         */
+        Thruster(int pin, int stablePower) : pin(pin), stablePower(stablePower), currentPower(0){
+            initThruster();
+        }
+
+        void initThruster(){
+            pinMode(this->pin, PWM_OUTPUT);
+            pwmSetMode(PWM_MODE_MS); // Entender e alterar se necessario
+            pwmSetClock(19200000 / (FREQUENCY * this->pwmRange));
+            pwmSetRange(pwmRange); // Entender e alterar se necessario
+            pwmWrite(this->pin, convertValue(0));
+        }
+
+        void move(double value){
+            int pwmValue = convertValue(value);
+            pwmWrite(pin, pwmValue);
+        }
+
+        int convertValue(double value){
+            // ENTENDER POR QUE ISSO SÓ FUNCIONOU COM DOUBLE !!!!!!!!!!!!!!
+            int minPWMus = 1100, maxPWMus = 1900;
+
+            double usefulPower = (value > 0 ? (100 - stablePower) : (100 + stablePower)) * (value / 100);
+            currentPower = max(-POWER_MAX, min(stablePower + usefulPower, POWER_MAX));
+
+            return (FREQUENCY / 1000000) * (minPWMus + ((maxPWMus - minPWMus) / 2) * (1 + (currentPower / 100))) * pwmRange;
+        }
+
+        void finishesThruster(){
+            pwmWrite(this->pin, 0);
+        }
+};
+
 class ThrustersControl{
     private:
-        array<Thruster, 6> thrusters;
+        vector<Thruster> thrusters;
 
     public:
         ThrustersControl(){
@@ -110,13 +142,10 @@ class ThrustersControl{
         }
 
         void initializeThrusters(){
-            for(int i = 0; i < 6; i++){
-                if(PINS[i] == 27 || PINS[i] == 22) Thruster thruster(PINS[i], -14);
-                else Thruster thruster(PINS[i], 0);
-
-                thrusters[i] = thruster;
+            for(int i = 0; i < PINS.size(); i++){
+                if(PINS[i] == 27 || PINS[i] == 22) thrusters.emplace_back(PINS[i], -14);
+                else thrusters.emplace_back(PINS[i], 0);
             }
-
             sleep_for(seconds(7));
         }
 
@@ -189,41 +218,8 @@ class ThrustersControl{
         void finish(){
             cout << "Turning off the thrusters..." << endl;
 
-            //finalizar
+            for(int i = 0; i < thrusters.size(); i++) thrusters[i].finishesThruster();
 
             cout << "Thrusters off" << endl;
-
-        }
-};
-
-class Thruster{
-    private:
-        int pin, stablePower, currentPower;
-
-    public:
-        /**
-         * @brief Initializes the object thruster
-         * 
-         * @param pin pin connected in the thrusters
-         * @param stablePower power for this thurster to stabilize the AUV (Vx: 0, Vy: 0, Vz: 0)
-         */
-        Thruster(int pin, int stablePower) : pin(pin), stablePower(stablePower){
-            currentPower = 0;
-        }
-
-        void initThruster(){
-
-        }
-
-        void move(double value){
-
-        }
-
-        int convertValue(double value){
-
-        }
-
-        void finishesThruster(){
-
         }
 };
