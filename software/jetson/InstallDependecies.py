69cad1a2e7987fe136fd6513b8ba322ecf93ac74
gustavo <gustavopiazdasilva2102@gmail.com>
2025-02-07 12:32:25 -0300

    instalador de dependencias

---
+++ software/jetson/InstallDependecies.py
@@ -0,0 +1,15 @@
+from os import system
+'''
+Caso queira adicionar comandos que precisem de sudo use o f'echo{password} | sudo -S ...'
+'''
+password = input("Digite sua senha sudo: ")
+
+comands = [
+    'pip install ultralytics',
+    f'echo {password} | sudo -S apt update',
+    f'echo {password} | sudo -S apt install libboost-all-dev',
+    f'echo {password} | sudo -S apt install nlohmann-json3-dev'
+]
+
+for comand in comands:
+    system(comand)
