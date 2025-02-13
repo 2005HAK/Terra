15041000966e4411b6534be0d97e7147983a86ae
2005HAK <hebertkubis15@gmail.com>
2025-02-13 11:55:47 -0300

    adição do código de recebimento de dados do Piaz ao código de navegação

--- software/raspberry_pi/src/navegation/navegation_5.0/src/yoloctrl.cpp
+++ software/raspberry_pi/src/navegation/navegation_5.0/src/yoloctrl.cpp
@@ -1,36 +1,92 @@
 #include <iostream>
-#include <array>
+#include <boost/asio.hpp>
+#include <nlohmann/json.hpp>
+#include <variant>
 #include <vector>
+#include <tuple>
 #include "utils.h"
 
-const int MAXDETECTIONS = 5;
+using boost::asio::ip::tcp;
+using json = nlohmann::json;
+
+using VarType = std::variant<double, int, std::string>;
 
 struct Object{
     string name;
-    int topLeftXY[2];
-    int downRightXY[2];
-    float confidance;
+    double topLeftXY[2];
+    double downRightXY[2];
+    double confidance;
+    int objectId;
 };
 
 class YoloCtrl{
     private:
         vector<Object> identifiedObjects;
-        string host = "0.0.0.0";
-        int port = 65432;
-        int socket;
 
     public:
         YoloCtrl(){
             cout << "Object YoloCtrl created" << endl;
-
-            // implementar a conexão
-
-            cout << "Socket opened" << endl;
         }
 
         void updateData(){
             while(1){
-                // implementar o recebimento de dados
+                receive();
+                sleep_for(milliseconds(350));
+            }
+        }
+
+        void process_json(const json& received_json) {
+            try{
+                if(received_json.contains("data")){
+                    for(const auto& obj : received_json["data"]){
+                        Object currentObject;
+
+                        if(obj.is_array() && obj.size() >= 6){
+                            currentObject.topLeftXY[0] = obj[0];
+                            currentObject.topLeftXY[1] = obj[1];
+                            currentObject.downRightXY[0] = obj[2];
+                            currentObject.downRightXY[1] = obj[3];
+                            currentObject.confidance = obj[4];
+                            currentObject.objectId = obj[5];
+                            currentObject.name = "Unknown";
+
+                            if(received_json.contains("names") && received_json["names"].contains(to_string(currentObject.objectId))) 
+                                currentObject.name = received_json["names"][to_string(currentObject.objectId)];
+
+                            identifiedObjects.emplace_back(currentObject);
+                        }
+                    }
+                }
+            }catch (const json::exception& e){
+                cerr << "Erro ao processar JSON: " << e.what() << endl;
+            }
+        }
+    
+        void receive(){  
+            try{
+                boost::asio::io_context io_context;
+                tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 65432));
+    
+                tcp::socket socket(io_context);
+                acceptor.accept(socket);
+
+                boost::asio::streambuf buffer;
+                boost::asio::read_until(socket, buffer, '\n');
+
+                istream is(&buffer);
+                string received_data;
+                getline(is, received_data);
+
+                try {
+                    json received_json = json::parse(received_data);
+                    
+                    process_json(received_json);
+                } catch (const json::parse_error& e) {
+                    cerr << "Erro ao interpretar JSON: " << e.what() << endl;
+                }
+                
+            } catch (exception& e) {
+                cerr << "Erro: " << e.what() << endl;
             }
         }
 
