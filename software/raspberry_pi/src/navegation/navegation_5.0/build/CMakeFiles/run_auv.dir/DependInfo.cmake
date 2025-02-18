fa8d43abc41925131b5f08a91ce735ab09f24379
2005HAK <hebertkubis15@gmail.com>
2025-02-18 18:48:25 -0300

    tentativa (1) falhou

---
+++ software/raspberry_pi/src/navegation/navegation_5.0/build/CMakeFiles/run_auv.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/equipeterra/Terra/software/raspberry_pi/src/navegation/navegation_5.0/src/navegation.cpp" "CMakeFiles/run_auv.dir/src/navegation.cpp.o" "gcc" "CMakeFiles/run_auv.dir/src/navegation.cpp.o.d"
+  "/home/equipeterra/Terra/software/raspberry_pi/src/navegation/navegation_5.0/src/sensors.cpp" "CMakeFiles/run_auv.dir/src/sensors.cpp.o" "gcc" "CMakeFiles/run_auv.dir/src/sensors.cpp.o.d"
+  "/home/equipeterra/Terra/software/raspberry_pi/src/navegation/navegation_5.0/src/thrusters_control.cpp" "CMakeFiles/run_auv.dir/src/thrusters_control.cpp.o" "gcc" "CMakeFiles/run_auv.dir/src/thrusters_control.cpp.o.d"
+  "/home/equipeterra/Terra/software/raspberry_pi/src/navegation/navegation_5.0/src/yoloctrl.cpp" "CMakeFiles/run_auv.dir/src/yoloctrl.cpp.o" "gcc" "CMakeFiles/run_auv.dir/src/yoloctrl.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
