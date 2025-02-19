c7751406d6bb771115e72361a1fc3ade0c0491ea
2005HAK <hebertkubis15@gmail.com>
2025-02-19 09:59:38 -0300

    problemas de sintaxe

---
+++ software/raspberry_pi/src/navegation/navegation_5.0/build/CMakeFiles/run_auv.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/equipeterra/Terra/software/raspberry_pi/src/navegation/navegation_5.0/src/auverror.cpp" "CMakeFiles/run_auv.dir/src/auverror.cpp.o" "gcc" "CMakeFiles/run_auv.dir/src/auverror.cpp.o.d"
+  "/home/equipeterra/Terra/software/raspberry_pi/src/navegation/navegation_5.0/src/main_navegation.cpp" "CMakeFiles/run_auv.dir/src/main_navegation.cpp.o" "gcc" "CMakeFiles/run_auv.dir/src/main_navegation.cpp.o.d"
+  "/home/equipeterra/Terra/software/raspberry_pi/src/navegation/navegation_5.0/src/navegation.cpp" "CMakeFiles/run_auv.dir/src/navegation.cpp.o" "gcc" "CMakeFiles/run_auv.dir/src/navegation.cpp.o.d"
+  "/home/equipeterra/Terra/software/raspberry_pi/src/navegation/navegation_5.0/src/sensors.cpp" "CMakeFiles/run_auv.dir/src/sensors.cpp.o" "gcc" "CMakeFiles/run_auv.dir/src/sensors.cpp.o.d"
+  "/home/equipeterra/Terra/software/raspberry_pi/src/navegation/navegation_5.0/src/thrustersctrl.cpp" "CMakeFiles/run_auv.dir/src/thrustersctrl.cpp.o" "gcc" "CMakeFiles/run_auv.dir/src/thrustersctrl.cpp.o.d"
+  "/home/equipeterra/Terra/software/raspberry_pi/src/navegation/navegation_5.0/src/utils.cpp" "CMakeFiles/run_auv.dir/src/utils.cpp.o" "gcc" "CMakeFiles/run_auv.dir/src/utils.cpp.o.d"
+  "/home/equipeterra/Terra/software/raspberry_pi/src/navegation/navegation_5.0/src/yoloctrl.cpp" "CMakeFiles/run_auv.dir/src/yoloctrl.cpp.o" "gcc" "CMakeFiles/run_auv.dir/src/yoloctrl.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ software/raspberry_pi/src/navegation/navegation_5.0/build/CMakeFiles/run_auv.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run_auv.dir/src/auverror.cpp.o"
+  "CMakeFiles/run_auv.dir/src/auverror.cpp.o.d"
+  "CMakeFiles/run_auv.dir/src/main_navegation.cpp.o"
+  "CMakeFiles/run_auv.dir/src/main_navegation.cpp.o.d"
+  "CMakeFiles/run_auv.dir/src/navegation.cpp.o"
+  "CMakeFiles/run_auv.dir/src/navegation.cpp.o.d"
+  "CMakeFiles/run_auv.dir/src/sensors.cpp.o"
+  "CMakeFiles/run_auv.dir/src/sensors.cpp.o.d"
+  "CMakeFiles/run_auv.dir/src/thrustersctrl.cpp.o"
+  "CMakeFiles/run_auv.dir/src/thrustersctrl.cpp.o.d"
+  "CMakeFiles/run_auv.dir/src/utils.cpp.o"
+  "CMakeFiles/run_auv.dir/src/utils.cpp.o.d"
+  "CMakeFiles/run_auv.dir/src/yoloctrl.cpp.o"
+  "CMakeFiles/run_auv.dir/src/yoloctrl.cpp.o.d"
+  "run_auv"
+  "run_auv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run_auv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
