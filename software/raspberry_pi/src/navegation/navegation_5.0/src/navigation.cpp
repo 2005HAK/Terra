27990849b12cfd1731e1d1f72516ff2cc6cfce9d
2005HAK <hebertkubis15@gmail.com>
2025-05-16 15:02:43 -0300

    verificação dos dados dos sensores

--- software/raspberry_pi/src/navegation/navegation_5.0/src/navigation.cpp
+++ software/raspberry_pi/src/navegation/navegation_5.0/src/navigation.cpp
@@ -137,12 +137,12 @@ AUVStateMachine::~AUVStateMachine(){
 void AUVStateMachine::sensorsData(){
     sensors->initialize();
 
-    /*
+    
     while(running){
         sensors->updateData();
 	sleep_for(milliseconds(100));
     }
-    */
+    
 }
 
 void AUVStateMachine::detectionData(){
