d8ad7f5e2fe5042230d3731c6817d29e55a96fe9
2005HAK <hebertkubis15@gmail.com>
2025-05-16 16:38:05 -0300

    correção do erro na rotação 6

--- software/raspberry_pi/src/navegation/navegation_5.0/src/navigation.cpp
+++ software/raspberry_pi/src/navegation/navegation_5.0/src/navigation.cpp
@@ -621,9 +621,8 @@ void AUVStateMachine::rotate(double angle, double errorAngle, Action action){
     
     while(fabs(oriCurrent[2] - oriInit[2]) < angle - errorAngle){
         int error = angle - errorAngle - fabs(oriCurrent[2] - oriInit[2]);
-        cout << "Rotation angle: " << angle - errorAngle << endl;
+        cout << "Error: " << error << endl;
         decision.value = error * 60;
-        cout << "Rotation: " << fabs(oriCurrent[2] - oriInit[2]) << endl;
         this->thrusters->defineAction(decision);
 
         oriCurrent = this->sensors->getOri();
