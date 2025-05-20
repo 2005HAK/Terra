cd1196789d276a658a67c063751a401270474e8a
2005HAK <hebertkubis15@gmail.com>
2025-05-20 19:23:52 -0300

    resolução do erro em utils

--- software/raspberry_pi/src/navegation/navegation_5.0/src/utils.cpp
+++ software/raspberry_pi/src/navegation/navegation_5.0/src/utils.cpp
@@ -10,13 +10,13 @@ string getTime(){
 
 string actionToString(Action action){
     if(Action::FORWARD == action) return "FORWARD";
-    if else(Action::BACKWARD == action) return "BACKWARD";
-    if else(Action::RIGHT == action) return "RIGHT";
-    if else(Action::LEFT == action) return "LEFT";
-    if else(Action::UP == action) return "UP";
-    if else(Action::DOWN == action) return "DOWN";
-    if else(Action::TURNRIGHT == action) return "TURNRIGHT";
-    if else(Action::TURNLEFT == action) return "TURNLEFT";
-    if else(Action::NONE == action) return "NONE";
+    else if(Action::BACKWARD == action) return "BACKWARD";
+    else if(Action::RIGHT == action) return "RIGHT";
+    else if(Action::LEFT == action) return "LEFT";
+    else if(Action::UP == action) return "UP";
+    else if(Action::DOWN == action) return "DOWN";
+    else if(Action::TURNRIGHT == action) return "TURNRIGHT";
+    else if(Action::TURNLEFT == action) return "TURNLEFT";
+    else if(Action::NONE == action) return "NONE";
     else return "UNKNOWN";
 }
